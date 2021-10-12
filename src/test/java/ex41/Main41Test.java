63fe98cfc8f0d347e46db1ec20e8654788def2a6
CailaC <caila.coates2010@gmail.com>
2021-10-11 20:40:15 -0400

    Commit

--- src/test/java/ex41/Main41Test.java
+++ src/test/java/ex41/Main41Test.java
@@ -1,9 +1,18 @@
 package ex41;
 
 import junit.framework.TestCase;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Main41Test extends TestCase {
 
-    public void testSortListAlphabetically() {
+
+
+    public void testSortListAlphabetically() throws IOException {
+        File expectedValue = new File("src/test/java/ex41/expected41_output");
+        File actual = new File("src/main/java/ex41/exercise41_input.txt");
+        assertEquals(FileUtils.readFileToByteArray(expectedValue),FileUtils.readFileToByteArray(actual));
     }
 }
